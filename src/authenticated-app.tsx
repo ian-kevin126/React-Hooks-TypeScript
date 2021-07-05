46607bfa21171f7d5f62e5c7d4dcf8a88aaeb326
ian_kevin126 <ian_kevin126@126.com>
2021-07-05 22:38:08 +0800

    feat: clear console

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -12,10 +12,8 @@ import { Button, Dropdown, Menu } from "antd";
  */
 export const AuthenticatedApp = () => {
   const { logout, user } = useAuth();
-  const value: any = undefined;
   return (
     <Container>
-      {value.NotExist}
       <Header between={true}>
         <HeaderLeft gap={true}>
           <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
